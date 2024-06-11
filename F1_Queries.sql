ef603283faedb7649ec780bfd1693cfb81bd4fbd
Dhruvin-Mistry <104815059+Dhruvin-Mistry@users.noreply.github.com>
2024-06-11 17:33:54 +0530

    Update F1_Queries.sql

--- F1_Queries.sql
+++ F1_Queries.sql
@@ -1,10 +1,9 @@
 --Query 1: Showing the First Wins of Drivers on the Current(2023) Grid 
 
-/*
-In this approach, I used the `ROW_NUMBER()` function to rank each driver's race wins by year.
+/* In this approach, I used the ROW_NUMBER() function to rank each driver's race wins by year.
 This ranking process was done within a CTE (Common Table Expression). Then, I filtered out the current 
 drivers based on specific criteria. After that, I joined the drivers table with the ranked drivers table,
-applying a filter to extract relevant drivers from the drivers table. By using a `LEFT JOIN`, I ensured that
+applying a filter to extract relevant drivers from the drivers table. By using a LEFT JOIN, I ensured that
 all initial drivers are included in the final results, even if they don't have any wins.*/
 
 
@@ -20,8 +19,10 @@ WITH RankedWins AS (
     FROM F1_Project.dbo.drivers$ d
     JOIN F1_Project.dbo.results$ res ON d.driverID = res.driverId
     JOIN F1_Project.dbo.races$ r ON res.raceId = r.raceId
-    WHERE d.forename IN ('Max', 'Sergio', 'Charles', 'Carlos', 'Oscar', 'Lando', 'Lewis', 'George', 'Fernando', 'Lance', 'Daniel', 'Yuki', 'Nico', 'Guanyu', 'Valtteri', 'Alexander', 'Logan', 'Esteban', 'Pierre')
-    AND d.number IN (33, 27, 81, 10, 11, 77, 22, 23, 55, 16, 3, 31, 14, 63, 24, 18, 4, 44, 2)
+    where CONCAT(d.forename,' ',d.surname) IN 
+('Lewis Hamilton', 'Fernando Alonso', 'Pierre Gasly', 'Nico Hülkenberg', 'Sergio Pérez', 'Daniel Ricciardo', 'Valtteri Bottas', 'Kevin Magnussen', 'Max Verstappen', 'Carlos Sainz', 'Esteban Ocon', 'Lance Stroll', 'Charles Leclerc', 'Lando Norris', 'George Russell', 'Alexander Albon', 'Yuki Tsunoda', 'Guanyu Zhou', 'Oscar Piastri', 'Logan Sargeant')
+
+
     AND res.position = 1
 	
 )
@@ -37,6 +38,9 @@ WHERE d.forename IN ('Max', 'Sergio', 'Charles', 'Carlos', 'Oscar', 'Lando', 'Le
 AND d.number IN (33, 27, 81, 10, 11, 77, 22, 23, 55, 16, 3, 31, 14, 63, 24, 18, 4, 44, 2,20)
 ;
 
+
+
+----------------------------------------------------------------------------------------------------------------------
 
 --Query 2: Ferrari's three Most Successful Circuit
 
@@ -58,6 +62,10 @@ where con.name = 'Ferrari' and res.position = 1
 group by c.name,r.name
 order by Wins desc
 
+
+
+----------------------------------------------------------------------------------------------------------------------
+
 --Query 3:List of all the Drivers and their Teams
 
 /*Since the drivers and constructors tables do not have any common columns to connect directly, 
@@ -65,12 +73,193 @@ I used another table that shares a common column with both to establish the conn
 I faced here was that the Drivers change their teams throughout their career. Hence I needed to fix a year 
 to display the driver and their team for that current year.*/
 
-select distinct d.forename,d.surname,c.name
+select distinct d.forename,d.surname,c.name as team 
 from F1_Project.dbo.drivers$ d
 join F1_Project.dbo.results$ r on d.driverId = r.driverId
 join F1_Project.dbo.constructors$ c on c.constructorId = r.constructorId
 join F1_Project.dbo.races$ ra on r.raceId = ra.raceId 
-WHERE d.forename IN ('Max', 'Sergio', 'Charles', 'Carlos', 'Oscar', 'Lando', 'Lewis', 'George', 'Fernando', 'Lance', 'Daniel', 'Yuki', 'Nico','Kevin', 'Guanyu', 'Valtteri', 'Alexander', 'Logan', 'Esteban', 'Pierre')
-AND d.number IN (33, 27, 81, 10, 11, 77, 22, 23, 55, 16, 3, 31, 14, 63, 24, 18, 4, 44, 2,20)
+where CONCAT(d.forename,' ',d.surname) IN 
+('Lewis Hamilton', 'Fernando Alonso', 'Pierre Gasly', 'Nico Hülkenberg', 'Sergio Pérez', 'Daniel Ricciardo', 'Valtteri Bottas', 'Kevin Magnussen', 'Max Verstappen', 'Carlos Sainz', 'Esteban Ocon', 'Lance Stroll', 'Charles Leclerc', 'Lando Norris', 'George Russell', 'Alexander Albon', 'Yuki Tsunoda', 'Guanyu Zhou', 'Oscar Piastri', 'Logan Sargeant')
 AND ra.year = 2023
 order by c.name
+
+
+
+----------------------------------------------------------------------------------------------------------------------
+
+--Query 4: Constructors and Drivers having the same nationality
+
+select distinct d.forename,d.surname,d.nationality ,c.name as team, c.nationality as TeamNationality
+from F1_Project.dbo.drivers$ d
+join F1_Project.dbo.results$ r on d.driverId = r.driverId
+join F1_Project.dbo.constructors$ c on c.constructorId = r.constructorId
+join F1_Project.dbo.races$ ra on r.raceId = ra.raceId 
+where d.nationality = c.nationality
+order by c.name
+
+
+
+----------------------------------------------------------------------------------------------------------------------
+
+--Query 5: All Circuits with max Constructors win i.e Which team won the most races at different circuits
+
+/*For this query, I divided the data processing into three steps. First, I calculated the total wins 
+for each constructor at each circuit, including the count of wins. Then, I created a separate query to 
+determine the maximum number of wins for each circuit, resulting in a table with circuit IDs and their 
+corresponding maximum wins. Finally, I combined these results to identify the constructor with the most 
+wins at each circuit by joining the tables on circuit ID and the maximum wins. */
+
+WITH CircuitWins AS (
+SELECT c.circuitId,c.name, c.location, con.name AS constructor_name, COUNT(re.position) AS Wins
+FROM F1_Project.dbo.circuits$ c
+JOIN F1_Project.dbo.races$ r ON c.circuitId = r.circuitId
+JOIN F1_Project.dbo.results$ re ON r.raceId = re.raceId
+JOIN F1_Project.dbo.constructors$ con ON con.constructorId = re.constructorId
+WHERE re.position = 1
+GROUP BY c.circuitId, c.name, c.location, con.name
+),
+MaxWins as(
+SELECT circuitId, MAX(Wins) AS MaxWins
+FROM CircuitWins
+GROUP BY circuitId
+)
+SELECT cw.circuitId, cw.name, cw.location, cw.Wins, cw.constructor_name
+FROM CircuitWins cw
+JOIN MaxWins mw ON cw.circuitId = mw.circuitId AND cw.Wins = mw.MaxWins
+ORDER BY cw.circuitId;
+
+
+
+----------------------------------------------------------------------------------------------------------------------
+
+--Query 6: Top 5 Circuits Having Most Races
+
+/*Simple Query showing top 5 circuits where most races are held from the beginning of Formula One*/
+
+select top 5 name, count(circuitId) as MaxRacesAcrossGlobe
+from  F1_Project.dbo.races$
+group by name
+order by MaxRacesAcrossGlobe desc
+
+
+
+----------------------------------------------------------------------------------------------------------------------
+
+--Query 7: Average position of each Driver in whichever race they finished and Constructor for a Particular year.
+
+/* Here I have created two CTEs, the first one gets the races of 2022 and the second one gets the driver and driver positions using the two tables of driver and driver_standings. Then next in the main query I joined both to get the drivers, their positions in the race year that I filtered. Then I used COALESCE to replace NULL with 20th position so that the average comes out to be more accurate. And used the AVG function to find the average position of the drivers who raced in 2022 season.*/
+
+with selected_races as(
+
+select raceId
+from F1_Project.dbo.races$
+where year = 2022),
+
+driver_pos as(
+select a.raceId,a.driverId, CONCAT(c.forename, ' ', c.surname) AS fullname, a.position
+from F1_Project.dbo.results$ a
+join F1_Project.dbo.driver_standings$ b
+on a.driverId = b.driverId
+join F1_Project.dbo.drivers$ c on a.driverId = c.driverId)
+
+select x.fullname, ROUND(AVG(COALESCE(x.position,20)),1) as avgr
+from driver_pos x join selected_races y on x.raceId = y.raceId
+group by x.fullname
+order by avg_pos
+
+
+/*Same approach as earlier one for constructors as well.*/
+
+with selected_races2 as(
+
+select raceId
+from F1_Project.dbo.races$
+where year = 2022),
+
+constructor_pos as(
+
+select a. raceId , a.constructorId,a.position
+from F1_Project.dbo.constructor_standings$ a
+)
+select z.name, ROUND(AVG(COALESCE(x.position,2)),1) as avg_pos
+from constructor_pos x 
+join selected_races2 y  on x.raceId = y.raceId
+join F1_Project.dbo.constructors$ z on x.constructorId = z.constructorId
+group by z.name
+order by avg_pos
+
+
+
+----------------------------------------------------------------------------------------------------------------------
+
+--Query 8: Fastest laps in all races from year 2021-2023
+
+/* A slightly tricky query wherein I had to spend a lot of time. In the fast_laps CTE, the query identifies the fastest lap for each race by joining the lap times with the minimum lap times calculated per race. The ROW_NUMBER() function ranks the laps within each race, ensuring only the fastest lap is highlighted. The problem here is that lap time precision is not provided. Hence the laptimes are similar for lot of drivers. In the second CTE, races within a specific year range are selected. In the main query, these two CTEs are joined with the drivers table, and only the fastest laps (ranked as 1) are displayed.  */
+
+
+
+WITH fast_laps AS (
+    SELECT lt.raceId, lt.time AS fast_lap, lt.driverId,
+           ROW_NUMBER() OVER (PARTITION BY lt.raceId ORDER BY lt.time) AS lap_rank
+    FROM F1_Project.dbo.lap_times$ lt
+    JOIN (
+        SELECT raceId, MIN(time) AS min_time
+        FROM F1_Project.dbo.lap_times$
+        GROUP BY raceId
+    ) AS min_lap 
+	ON lt.raceId = min_lap.raceId AND lt.time = min_lap.min_time
+),
+
+select_races AS (
+    SELECT raceId, name, year
+    FROM F1_Project.dbo.races$ 
+    WHERE year BETWEEN 2021 AND 2023
+)
+
+SELECT b.name, b.year, CONVERT(VARCHAR(12), fast_lap, 114) as fastest_lap , CONCAT(c.forename, ' ', c.surname) AS fullname
+FROM fast_laps a 
+JOIN select_races b ON a.raceId = b.raceId
+JOIN F1_Project.dbo.drivers$ c ON a.driverId = c.driverId
+WHERE a.lap_rank = 1
+ORDER BY b.year;
+
+
+
+----------------------------------------------------------------------------------------------------------------------
+
+--Query 9: Top 5 drivers with most Podium Finishes
+
+/*I executed a straightforward query linking the drivers to the result table to retrieve their positions and names. Then, I applied a filter to only include positions 1 through 3, representing podium finishes, sorted them, and narrowed down the list to the top 5 performers. */
+
+select top 5 r.driverId, CONCAT(d.forename,' ',d.surname) as fullname, count(r.position) as podiums
+from F1_Project.dbo.results$ r
+join F1_Project.dbo.drivers$ d on r.driverId = d.driverId
+where position between 1 and 3
+group by r.driverId, CONCAT(d.forename,' ',d.surname)
+order by podiums desc
+
+
+
+----------------------------------------------------------------------------------------------------------------------
+
+--Query 10 Listing All the races where a driver finished within a certain range. 
+
+/* A query where I used CTEs to create CONCAT of first name and last name for a driver so that it is easier to find him. Next I join three tables using their respective primary keys and added the filter of position and driver.
+
+*/
+
+with drivers as(
+
+select d.driverId, CONCAT(d.forename, ' ' ,d.surname) as fullname
+from F1_Project.dbo.drivers$ d
+
+)
+select b.name, COALESCE (a.position,20) as position, b.year
+from F1_Project.dbo.results$ a
+join F1_Project.dbo.races$ b 
+on a.raceId = b.raceId
+join drivers c
+on a.driverId = c.driverId
+where c.fullname = 'Lewis Hamilton'
+and a.position between 1 and 10 
+
